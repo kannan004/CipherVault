ced889b5630099e6501378285bceae7e6063613d
Kannan <kannan820408@gmail.com>
2026-07-17 04:40:43 +0000

    feat(database): integrate SQLite with Flask application

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,41 @@
 from flask import Flask
+from config import Config
+from database.database import db
+from models.user import User
 from services.encryption import encrypt_file, decrypt_file
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return '🔐 CipherVault Backend Running'
+app.config.from_object(Config)
 
-@app.route('/test')
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route("/")
+def home():
+    return "🔐 CipherVault Backend Running"
+
+@app.route("/test")
 def test():
-    message = b'Hello CipherVault!'
+    message = b"Hello CipherVault!"
+
     encrypted = encrypt_file(message)
     decrypted = decrypt_file(encrypted)
 
-    return f'''
+    return f"""
     <h2>CipherVault Test</h2>
-    <b>Original:</b> {message.decode()}<br><br>
-    <b>Encrypted:</b> {encrypted.decode()}<br><br>
-    <b>Decrypted:</b> {decrypted.decode()}
-    '''
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    <b>Original:</b><br>
+    {message.decode()}<br><br>
+
+    <b>Encrypted:</b><br>
+    {encrypted.decode()}<br><br>
+
+    <b>Decrypted:</b><br>
+    {decrypted.decode()}
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ backend/models/user.py
@@ -0,0 +1,23 @@
+from database.database import db
+from datetime import datetime
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    email = db.Column(
+        db.String(120),
+        unique=True,
+        nullable=False
+    )
+
+    password = db.Column(
+        db.String(255),
+        nullable=False
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow
+    )
